f6904bc9b87585c27a0dbee5246a34da8141d729
Owen Newlove <owen.newlove@gmail.com>
2022-10-13 14:32:14 -0400

    started reworking the seed data and seed file in the utils

---
+++ utils/seed.js
@@ -0,0 +1,9 @@
+const connection = require('../config/connection');
+const { Thought, User } = require('../models');
+
+const thoughtsData = require('./thoughts.json')
+
+const usersData = require('./users.json')
+
+
+
